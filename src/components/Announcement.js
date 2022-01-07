942c82cddca8f10666a6edc654a0655cf6833e93
Aqsa Rafiq <aqsarafique742@gmail.com>
2022-01-07 23:03:08 +0500

    Create Announcement.js

---
+++ src/components/Announcement.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+height: 30px;
+background-color: black;
+color: white;
+display: flex;
+align-items: center;
+justify-content: center;
+font-size: 14px;
+font-weight: 500;
+`
+const Announcement = () => {
+    return (
+        <Container>
+            Super Deal! Free Shipping on Orders over 5000 Pkr.
+        </Container>
+    )
+}
+
+export default Announcement
