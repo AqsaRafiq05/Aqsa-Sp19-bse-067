95873c30bfb0795ee95ed068e24ec90e6d023757
Aqsa Rafiq <aqsarafique742@gmail.com>
2022-01-07 23:03:36 +0500

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,80 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "./background.jpg",
+    title: "EARINGS COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "./Slide4.jpg",
+    title: "RINGS COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "./Slide5.jpg",
+    title: "NECLACE COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "./category1.jpg",
+    title: "Earings",
+    cat:"earing",
+  },
+  {
+    id: 2,
+    img: "./category2.jpg",
+    title: "Rings",
+    cat:"rings ",
+   
+  },
+  {
+    id: 3,
+    img:"./category3.jpg" ,
+    title: "Necklaces",
+    cat:"neclace ",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "./ear 6.jpg",
+  },
+  {
+    id: 2,
+    img: "./ring2.jpg",
+  },
+  {
+    id: 3,
+    img: "./Nec4.jpg",
+  },
+  {
+    id: 4,
+    img:"./ear 4.jpg",
+  },
+  {
+    id: 5,
+    img: "./ring1.jpg",
+  },
+  {
+    id: 6,
+    img: "./Nec3.jpg",
+  },
+  {
+    id: 7,
+    img: "./ring3.jpg",
+  },
+  {
+    id: 8,
+    img: "./ear 7.jpg",
+  },
+];
