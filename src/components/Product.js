2c791362b63fb5af32b94d6c39a653bca6ec4acb
Aqsa Rafiq <aqsarafique742@gmail.com>
2022-01-07 23:03:31 +0500

    Create Product.js

---
+++ src/components/Product.js
@@ -0,0 +1,76 @@
+
+  import styled from "styled-components";
+  
+  const Info = styled.div`
+    opacity: 0;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.2);
+    z-index: 3;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: all 0.5s ease;
+    cursor: pointer;
+  `;
+  
+  const Container = styled.div`
+    flex: 1;
+    margin: 5px;
+    min-width: 280px;
+    height: 350px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #f5fbfd;
+    position: relative;
+  
+    &:hover ${Info}{
+      opacity: 1;
+    }
+  `;
+  
+  
+  const Image = styled.img`
+    height:350px;
+    width:280px;
+    z-index: 2;
+    object-fit: cover;
+  `;
+  
+  const Icon = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 10px;
+    transition: all 0.5s ease;
+    &:hover {
+      background-color: #e9f5f5;
+      transform: scale(1.1);
+    }
+  `;
+  
+  const Product = ({ item }) => {
+    return (
+      <Container>
+        <Image src={item.img} />
+        <Info>
+          <Icon>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
+  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+</svg>
+          </Icon>
+    
+        </Info>
+      </Container>
+    );
+  };
+  
+  export default Product;
